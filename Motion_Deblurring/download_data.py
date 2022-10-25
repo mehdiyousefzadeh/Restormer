200fb99d9ba8438cafda33c0c634177b8c24010d
mehdiyousefzadeh <47357813+mehdiyousefzadeh@users.noreply.github.com>
2022-10-25 13:55:20 +0330

    Update download_data.py

--- Motion_Deblurring/download_data.py
+++ Motion_Deblurring/download_data.py
@@ -15,8 +15,8 @@ parser.add_argument('--dataset', type=str, default='GoPro', help='all, GoPro, HI
 args = parser.parse_args()
 
 ### Google drive IDs ######
-GoPro_train = '1zgALzrLCC_tcXKu_iHQTHukKUVT1aodI'      ## https://drive.google.com/file/d/1zgALzrLCC_tcXKu_iHQTHukKUVT1aodI/view?usp=sharing
-GoPro_test  = '1k6DTSHu4saUgrGTYkkZXTptILyG9RRll'      ## https://drive.google.com/file/d/1k6DTSHu4saUgrGTYkkZXTptILyG9RRll/view?usp=sharing
+GoPro_train = '1FsxWHZTzVrb2hxWvUVGJb_3huoh4YSrb'      ## https://drive.google.com/file/d/1zgALzrLCC_tcXKu_iHQTHukKUVT1aodI/view?usp=sharing
+GoPro_test  = '12Mi4uQrKwMhy59cnsfFtMarQA8RRxGDQ'      ## https://drive.google.com/file/d/1k6DTSHu4saUgrGTYkkZXTptILyG9RRll/view?usp=sharing
 HIDE_test = '1XRomKYJF1H92g1EuD06pCQe4o6HlwB7A'        ## https://drive.google.com/file/d/1XRomKYJF1H92g1EuD06pCQe4o6HlwB7A/view?usp=sharing
 RealBlurR_test = '1glgeWXCy7Y0qWDc0MXBTUlZYJf8984hS'   ## https://drive.google.com/file/d/1glgeWXCy7Y0qWDc0MXBTUlZYJf8984hS/view?usp=sharing
 RealBlurJ_test = '1Rb1DhhXmX7IXfilQ-zL9aGjQfAAvQTrW'   ## https://drive.google.com/file/d/1Rb1DhhXmX7IXfilQ-zL9aGjQfAAvQTrW/view?usp=sharing
@@ -28,7 +28,7 @@ for data in args.data.split('-'):
         print('GoPro Training Data!')
         os.makedirs(os.path.join('Datasets', 'Downloads'), exist_ok=True)
         # gdown.download(id=GoPro_train, output='Datasets/Downloads/train.zip', quiet=False)
-        os.system(f'gdrive download {GoPro_train} --path Datasets/Downloads/')
+        os.system(f'gdown {GoPro_train} -O Datasets/Downloads/')
         print('Extracting GoPro data...')
         shutil.unpack_archive('Datasets/Downloads/train.zip', 'Datasets/Downloads')
         os.rename(os.path.join('Datasets', 'Downloads', 'train'), os.path.join('Datasets', 'Downloads', 'GoPro'))
@@ -38,7 +38,7 @@ for data in args.data.split('-'):
         if dataset == 'all' or dataset == 'GoPro':
             print('GoPro Testing Data!')
             # gdown.download(id=GoPro_test, output='Datasets/test.zip', quiet=False)
-            os.system(f'gdrive download {GoPro_test} --path Datasets/')
+            os.system(f'gdown {GoPro_test} -O Datasets/')
             print('Extracting GoPro Data...')
             shutil.unpack_archive('Datasets/test.zip', 'Datasets')
             os.remove('Datasets/test.zip')
@@ -46,7 +46,7 @@ for data in args.data.split('-'):
         if dataset == 'all' or dataset == 'HIDE':
             print('HIDE Testing Data!')
             # gdown.download(id=HIDE_test, output='Datasets/test.zip', quiet=False)
-            os.system(f'gdrive download {HIDE_test} --path Datasets/')
+            os.system(f'gdown {HIDE_test} -O Datasets/')
             print('Extracting HIDE Data...')
             shutil.unpack_archive('Datasets/test.zip', 'Datasets')
             os.remove('Datasets/test.zip')
@@ -54,7 +54,7 @@ for data in args.data.split('-'):
         if dataset == 'all' or dataset == 'RealBlur_R':
             print('RealBlur_R Testing Data!')
             # gdown.download(id=RealBlurR_test, output='Datasets/test.zip', quiet=False)
-            os.system(f'gdrive download {RealBlurR_test} --path Datasets/')
+            os.system(f'gdown {RealBlurR_test} -O Datasets/')
             print('Extracting RealBlur_R Data...')
             shutil.unpack_archive('Datasets/test.zip', 'Datasets')
             os.remove('Datasets/test.zip')
@@ -62,7 +62,7 @@ for data in args.data.split('-'):
         if dataset == 'all' or dataset == 'RealBlur_J':
             print('RealBlur_J testing Data!')
             # gdown.download(id=RealBlurJ_test, output='Datasets/test.zip', quiet=False)
-            os.system(f'gdrive download {RealBlurJ_test} --path Datasets/')
+            os.system(f'gdown {RealBlurJ_test} -O Datasets/')
             print('Extracting RealBlur_J Data...')
             shutil.unpack_archive('Datasets/test.zip', 'Datasets')
             os.remove('Datasets/test.zip')
